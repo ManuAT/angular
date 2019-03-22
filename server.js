e548c45fae97f7a4fa5cf7919ea507d05940e446
Manu A T <ManuAT@users.noreply.github.com>
2019-03-23 02:47:15 +0530

    dist added

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ mongoose.connect(database.remoteUrl,(err)=>{
 }); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
 
 app.use(cors());
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static('dist/lms'));
+
+app.get('/',(req,res)=>{
+res.sendFile('dist/lms/index.html');
+})
+
+// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
